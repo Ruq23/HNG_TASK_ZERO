34095094fcffb633f8a751dcebc410af9ae15dd7
“Ruq23” <daruq20@gmail.com>
2022-10-29 17:32:25 +0100

    one

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended: true}));
 app.get('/', (req, res) => {
     const age = '22'
     const bio = "I am a hardworking, articulate and ambitious individual, i am passionate about Web Development and i am excited to join the HNG Team."
-    const slackUsername = "Faruq"
+    const slackUsername = "Faruq22"
     const backend = true
 
     res.send(`{"slackUsername": "${slackUsername}", "backend": ${backend}, "age": ${age}, "bio": "${bio}"}`)
